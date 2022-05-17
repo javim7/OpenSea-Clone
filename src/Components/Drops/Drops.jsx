d9853f4e0a4821bb102b13f548dd81427b83551b
javim7 <rjmombiela@gmail.com>
2022-05-16 19:05:29 -0600

    empezando los carruseles

--- src/Components/Drops/Drops.jsx
+++ src/Components/Drops/Drops.jsx
@@ -12,25 +12,25 @@ import Vogue from './vogue.jpg'
 function Drops() {
   const promoCards = [
     {
-      title: '`Everybody` NFT Collection', id: 'vogue', description: 'A collection by Vogue Singapore, in collaboration The MetaArt Club', src: { Vogue },
+      title: '`Everybody` NFT Collection', id: 'vogue', description: 'A collection by Vogue Singapore, in collaboration The MetaArt Club', src: Vogue,
     },
     {
-      title: 'SuperGrannies of Korogocho', id: 'grannies', description: 'The lastest collection from ART3', src: { Grannies },
+      title: 'SuperGrannies of Korogocho', id: 'grannies', description: 'The lastest collection from ART3', src: Grannies,
     },
     {
-      title: 'NFTSTAR Fan Pass: Son Heung-Min', id: 'star', description: 'IRL and web3 empowerment for fans & holders', src: { Star },
+      title: 'NFTSTAR Fan Pass: Son Heung-Min', id: 'star', description: 'IRL and web3 empowerment for fans & holders', src: Star,
     },
     {
-      title: 'Signs of the Times', id: 'sign', src: { Sign },
+      title: 'Signs of the Times', id: 'sign', description: '', src: Sign,
     },
     {
-      title: 'Surge Passport NFT', id: 'surge', description: 'Onborading the next gen Web3: 5,000 utility tokens', src: { Surge },
+      title: 'Surge Passport NFT', id: 'surge', description: 'Onborading the next gen Web3: 5,000 utility tokens', src: Surge,
     },
     {
-      title: 'wackies', id: 'wackie', src: { Wackie },
+      title: 'wackies', id: 'wackie', description: '', src: Wackie,
     },
     {
-      title: 'Powerful Iceland', id: 'iceland', src: { Iceland },
+      title: 'Powerful Iceland', id: 'iceland', description: '', src: Iceland,
     },
   ]
 
@@ -48,7 +48,37 @@ function Drops() {
               </button>
               <div className="carousel">
                 <div className="carouselSty">
-
+                  {
+                    promoCards.map((card) => (
+                      <div className="fullCardCont">
+                        <div>
+                          <div className="fullCardCont2">
+                            <a className="fullCardLInk" href="https://opensea.io/collection/incaptivity" style={{ backgroundImage: `url(${card.src})` }}>
+                              <div className="upperCardCont">
+                                <div className="upperCardCont2">
+                                  <div className="upperCardCont3">
+                                    <div className="liveBtnCont">
+                                      <p className="liveBtn">Live</p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="lowerCardCont">
+                                <div className="lowerCard">
+                                  { card.title }
+                                </div>
+                                {
+                                  card.description.length > 0
+                                    ? <p className="cardDescription">{ card.description}</p>
+                                    : null
+                                }
+                              </div>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    ))
+                  }
                 </div>
               </div>
               <button type="button" className="rightSlide" aria-label="next slide">
@@ -56,9 +86,7 @@ function Drops() {
                   <ChevronRightIcon className="rightIcon" />
                 </div>
               </button>
-              <ul className="listaBtns">
-
-              </ul>
+              <ul className="listaBtns" />
             </div>
           </div>
         </div>
--- src/Components/Trending/Trending.jsx
+++ src/Components/Trending/Trending.jsx
@@ -1,9 +1,24 @@
+/* eslint-disable object-curly-newline */
 import './Trending.css'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 
+import unigrids from './unigrids.png'
+import skullbot from './skullbot.jpg'
+import skullbotL from './skullbotL.png'
+
 function Trending() {
+  const cards = [
+    { title: 'Unigrids by Zeblocks', by: 'ArtBlocks_Admin', description: 'Unigrids by Zeblocks - Art Blocks Curated', src: unigrids, logo: unigrids },
+    { title: 'Unigrids by Zeblocks', by: 'ArtBlocks_Admin', description: 'Unigrids by Zeblocks - Art Blocks Curated', src: unigrids, logo: unigrids },
+    { title: 'Unigrids by Zeblocks', by: 'ArtBlocks_Admin', description: 'Unigrids by Zeblocks - Art Blocks Curated', src: unigrids, logo: unigrids },
+    { title: 'Skullbot Biker Gang', by: 'Skullbots', description: 'Skullbot Biker Gang is a Couterculture Movement. A Rebellious Organization of 3,666...', src: skullbot, logo: skullbotL },
+    { title: 'Unigrids by Zeblocks', by: 'ArtBlocks_Admin', description: 'Unigrids by Zeblocks - Art Blocks Curated', src: unigrids, logo: unigrids },
+    { title: 'Unigrids by Zeblocks', by: 'ArtBlocks_Admin', description: 'Unigrids by Zeblocks - Art Blocks Curated', src: unigrids, logo: unigrids },
+    { title: 'Unigrids by Zeblocks', by: 'ArtBlocks_Admin', description: 'Unigrids by Zeblocks - Art Blocks Curated', src: unigrids, logo: unigrids },
+  ]
+
   return (
     <section className="TrendingContainer">
       <div className="h2Cont">
@@ -19,23 +34,60 @@ function Trending() {
         <div className="carCont2">
           <div className="carCont3">
             <button type="button" className="lSlide" aria-label="prev slide">
-                <div aria-hidden="true" className="lIconCont">
-                  <ChevronLeftIcon className="lIcon" />
-                </div>
-              </button>
-              <div className="car">
-                <div className="carSty">
-
-                </div>
+              <div aria-hidden="true" className="lIconCont">
+                <ChevronLeftIcon className="lIcon" />
               </div>
-              <button type="button" className="rSlide" aria-label="next slide">
-                <div aria-hidden="true" className="rIconCont">
-                  <ChevronRightIcon className="rIcon" />
-                </div>
-              </button>
-              <ul className="listBtns">
-
-              </ul>
+            </button>
+            <div className="car">
+              <div className="carSty">
+                {
+                  cards.map((card) => (
+                    <div className="fullCCont">
+                      <div>
+                        <div className="fullCCont2">
+                          <a className="fullCLink" href="https://opensea.io/collection/unigrids-by-zeblocks">
+                            <div className="upperCCont">
+                              <img src={card.src} alt="cardImg" className="cImgT" />
+                            </div>
+                            <div className="lowerCCont">
+                              <div className="cLogoCont">
+                                <img src={card.logo} alt="cardLogo" className="cLogoT" />
+                              </div>
+                              <div className="titleCardContT">
+                                <div className="titleCT">
+                                  {card.title}
+                                </div>
+                              </div>
+                              <div className="byCContT">
+                                <p className="byTxtT">by</p>
+                                <div className="authorCCont">
+                                  <a className="authotCLink" href="https://opensea.io/ArtBlocks_Admin">
+                                    {card.by}
+                                  </a>
+                                </div>
+                              </div>
+                              <span className="descripctionContC">
+                                <span className="descriptcionCT">
+                                  {card.description}
+                                </span>
+                              </span>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+            <button type="button" className="rSlide" aria-label="next slide">
+              <div aria-hidden="true" className="rIconCont">
+                <ChevronRightIcon className="rIcon" />
+              </div>
+            </button>
+            <ul className="listBtns">
+            
+            </ul>
           </div>
         </div>
       </div>
